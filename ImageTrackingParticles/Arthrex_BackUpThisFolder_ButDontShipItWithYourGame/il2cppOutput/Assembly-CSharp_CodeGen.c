8599574f29be180d01a5c4cb625cb043907fc8fa
Feerojkumar <140662095+Feerojkumar@users.noreply.github.com>
2023-08-24 23:45:13 +0530

    My 1st Commit

---
+++ ImageTrackingParticles/Arthrex_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,165 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 UnityEngine.GameObject AnchorCreator::get_AnchorPrefab()
+extern void AnchorCreator_get_AnchorPrefab_mBBAF7805E9D9F8D79408EE642D153BC76306B25A (void);
+// 0x00000002 System.Void AnchorCreator::set_AnchorPrefab(UnityEngine.GameObject)
+extern void AnchorCreator_set_AnchorPrefab_m53CC9CC3022C713826B44536B48B462C8A1FACFF (void);
+// 0x00000003 System.Void AnchorCreator::RemoveAllAnchors()
+extern void AnchorCreator_RemoveAllAnchors_m6BA302DD9EECA47969FD3F6E5397441DCB09ED21 (void);
+// 0x00000004 System.Void AnchorCreator::Awake()
+extern void AnchorCreator_Awake_m8820A3F157354D7EDED7B2D7CE2BFA42844F1F8E (void);
+// 0x00000005 System.Void AnchorCreator::Update()
+extern void AnchorCreator_Update_mA0A8BCCBAB0AE50DB087524E6273D11F1D6456D0 (void);
+// 0x00000006 System.Void AnchorCreator::.ctor()
+extern void AnchorCreator__ctor_m702B01425680D7FD1C9272A423AAF8A913E5476E (void);
+// 0x00000007 System.Void AnchorCreator::.cctor()
+extern void AnchorCreator__cctor_mDE519BED5D639DA8CD4FA7E389C3D97DF4BA54C4 (void);
+// 0x00000008 System.Single ARFeatheredPlaneMeshVisualizer::get_featheringWidth()
+extern void ARFeatheredPlaneMeshVisualizer_get_featheringWidth_m14D3A8BE3E9A745E6FD525B19ADDC892B8399B4D (void);
+// 0x00000009 System.Void ARFeatheredPlaneMeshVisualizer::set_featheringWidth(System.Single)
+extern void ARFeatheredPlaneMeshVisualizer_set_featheringWidth_mD616A09A3B426EA5DE1FA37334DD194E43EEC110 (void);
+// 0x0000000A System.Void ARFeatheredPlaneMeshVisualizer::Awake()
+extern void ARFeatheredPlaneMeshVisualizer_Awake_mC5DB0414A2514BF4851266C25141C903F0AC57BA (void);
+// 0x0000000B System.Void ARFeatheredPlaneMeshVisualizer::OnEnable()
+extern void ARFeatheredPlaneMeshVisualizer_OnEnable_m8781C85CFED871C8A81A5B88DB1031856E0FC9F0 (void);
+// 0x0000000C System.Void ARFeatheredPlaneMeshVisualizer::OnDisable()
+extern void ARFeatheredPlaneMeshVisualizer_OnDisable_m2343B05B1A8F14BAD4DD516C584281B66FE6A4E8 (void);
+// 0x0000000D System.Void ARFeatheredPlaneMeshVisualizer::ARPlane_boundaryUpdated(UnityEngine.XR.ARFoundation.ARPlaneBoundaryChangedEventArgs)
+extern void ARFeatheredPlaneMeshVisualizer_ARPlane_boundaryUpdated_mB3D9BBD14EA1FE3ECDBACC2DB89C1B110B8B6B5F (void);
+// 0x0000000E System.Void ARFeatheredPlaneMeshVisualizer::GenerateBoundaryUVs(UnityEngine.Mesh)
+extern void ARFeatheredPlaneMeshVisualizer_GenerateBoundaryUVs_mF756D3C1F7925A69CD8C7C8CCE56209AB321FEF5 (void);
+// 0x0000000F System.Void ARFeatheredPlaneMeshVisualizer::.ctor()
+extern void ARFeatheredPlaneMeshVisualizer__ctor_m9A77651BCAE58AA0B994FFF6C6B63B1CFF2729F6 (void);
+// 0x00000010 System.Void ARFeatheredPlaneMeshVisualizer::.cctor()
+extern void ARFeatheredPlaneMeshVisualizer__cctor_m56285A000EE27A1631BE9C5BCDA5C376E5D133C3 (void);
+// 0x00000011 System.Void PlaceTrackedImages::Awake()
+extern void PlaceTrackedImages_Awake_m875A81CA90E727B87965D5FA603AE8AFCE931A78 (void);
+// 0x00000012 System.Void PlaceTrackedImages::OnEnable()
+extern void PlaceTrackedImages_OnEnable_mA30F29043B9F5A6995F2CBA8D1340C8F42667EE4 (void);
+// 0x00000013 System.Void PlaceTrackedImages::OnDisable()
+extern void PlaceTrackedImages_OnDisable_m401542722AB2DC781D193CFD7E2E8F8873C8AEF6 (void);
+// 0x00000014 System.Void PlaceTrackedImages::OnTrackedImagesChanged(UnityEngine.XR.ARFoundation.ARTrackedImagesChangedEventArgs)
+extern void PlaceTrackedImages_OnTrackedImagesChanged_m9B9BAD9C115836AB3E18F38AA6B21EF6D951D463 (void);
+// 0x00000015 System.Void PlaceTrackedImages::.ctor()
+extern void PlaceTrackedImages__ctor_m6ED9F36190AAA71939672E2EA701CDB2FE277260 (void);
+// 0x00000016 System.Void ImageTrackingParticles::OnEnable()
+extern void ImageTrackingParticles_OnEnable_m2DFE1C5027F0A645889223AF7CCE7D5D0FD19E16 (void);
+// 0x00000017 System.Void ImageTrackingParticles::OnDisable()
+extern void ImageTrackingParticles_OnDisable_mB693E76CAE113B54B9E03B60C42244A16C6972A9 (void);
+// 0x00000018 System.Void ImageTrackingParticles::OnTrackedImagesChanged(UnityEngine.XR.ARFoundation.ARTrackedImagesChangedEventArgs)
+extern void ImageTrackingParticles_OnTrackedImagesChanged_m549C02721FF669B90B4CB62F0D1B3D7201349CD2 (void);
+// 0x00000019 System.Void ImageTrackingParticles::CreateParticles(UnityEngine.XR.ARFoundation.ARTrackedImage)
+extern void ImageTrackingParticles_CreateParticles_m938A4BEEEEB6C72F568CD93A876BE40A8AC8B95E (void);
+// 0x0000001A System.Void ImageTrackingParticles::UpdateParticlePosition(UnityEngine.XR.ARFoundation.ARTrackedImage)
+extern void ImageTrackingParticles_UpdateParticlePosition_mCF63A967A9DB198A762718DED6E9675CEA633DBE (void);
+// 0x0000001B System.Void ImageTrackingParticles::DestroyParticle(UnityEngine.XR.ARFoundation.ARTrackedImage)
+extern void ImageTrackingParticles_DestroyParticle_m4F2ADE315BD7BCC060DB74B54BEB05E1227B2E0E (void);
+// 0x0000001C System.Void ImageTrackingParticles::.ctor()
+extern void ImageTrackingParticles__ctor_m9CEF82DA61D73F1DC11CBA8A61CC04EE94F4E711 (void);
+// 0x0000001D System.Void ParticleUserInteraction::Start()
+extern void ParticleUserInteraction_Start_m1AE227CE17E1F4541F224A899852B7FC64E0F8BF (void);
+// 0x0000001E System.Void ParticleUserInteraction::Update()
+extern void ParticleUserInteraction_Update_mF559967296F128C26B1C4D155D3F670B0FF366B2 (void);
+// 0x0000001F System.Void ParticleUserInteraction::.ctor()
+extern void ParticleUserInteraction__ctor_mBF4627C05D9F198643C45E345E405388B27BD6ED (void);
+static Il2CppMethodPointer s_methodPointers[31] = 
+{
+	AnchorCreator_get_AnchorPrefab_mBBAF7805E9D9F8D79408EE642D153BC76306B25A,
+	AnchorCreator_set_AnchorPrefab_m53CC9CC3022C713826B44536B48B462C8A1FACFF,
+	AnchorCreator_RemoveAllAnchors_m6BA302DD9EECA47969FD3F6E5397441DCB09ED21,
+	AnchorCreator_Awake_m8820A3F157354D7EDED7B2D7CE2BFA42844F1F8E,
+	AnchorCreator_Update_mA0A8BCCBAB0AE50DB087524E6273D11F1D6456D0,
+	AnchorCreator__ctor_m702B01425680D7FD1C9272A423AAF8A913E5476E,
+	AnchorCreator__cctor_mDE519BED5D639DA8CD4FA7E389C3D97DF4BA54C4,
+	ARFeatheredPlaneMeshVisualizer_get_featheringWidth_m14D3A8BE3E9A745E6FD525B19ADDC892B8399B4D,
+	ARFeatheredPlaneMeshVisualizer_set_featheringWidth_mD616A09A3B426EA5DE1FA37334DD194E43EEC110,
+	ARFeatheredPlaneMeshVisualizer_Awake_mC5DB0414A2514BF4851266C25141C903F0AC57BA,
+	ARFeatheredPlaneMeshVisualizer_OnEnable_m8781C85CFED871C8A81A5B88DB1031856E0FC9F0,
+	ARFeatheredPlaneMeshVisualizer_OnDisable_m2343B05B1A8F14BAD4DD516C584281B66FE6A4E8,
+	ARFeatheredPlaneMeshVisualizer_ARPlane_boundaryUpdated_mB3D9BBD14EA1FE3ECDBACC2DB89C1B110B8B6B5F,
+	ARFeatheredPlaneMeshVisualizer_GenerateBoundaryUVs_mF756D3C1F7925A69CD8C7C8CCE56209AB321FEF5,
+	ARFeatheredPlaneMeshVisualizer__ctor_m9A77651BCAE58AA0B994FFF6C6B63B1CFF2729F6,
+	ARFeatheredPlaneMeshVisualizer__cctor_m56285A000EE27A1631BE9C5BCDA5C376E5D133C3,
+	PlaceTrackedImages_Awake_m875A81CA90E727B87965D5FA603AE8AFCE931A78,
+	PlaceTrackedImages_OnEnable_mA30F29043B9F5A6995F2CBA8D1340C8F42667EE4,
+	PlaceTrackedImages_OnDisable_m401542722AB2DC781D193CFD7E2E8F8873C8AEF6,
+	PlaceTrackedImages_OnTrackedImagesChanged_m9B9BAD9C115836AB3E18F38AA6B21EF6D951D463,
+	PlaceTrackedImages__ctor_m6ED9F36190AAA71939672E2EA701CDB2FE277260,
+	ImageTrackingParticles_OnEnable_m2DFE1C5027F0A645889223AF7CCE7D5D0FD19E16,
+	ImageTrackingParticles_OnDisable_mB693E76CAE113B54B9E03B60C42244A16C6972A9,
+	ImageTrackingParticles_OnTrackedImagesChanged_m549C02721FF669B90B4CB62F0D1B3D7201349CD2,
+	ImageTrackingParticles_CreateParticles_m938A4BEEEEB6C72F568CD93A876BE40A8AC8B95E,
+	ImageTrackingParticles_UpdateParticlePosition_mCF63A967A9DB198A762718DED6E9675CEA633DBE,
+	ImageTrackingParticles_DestroyParticle_m4F2ADE315BD7BCC060DB74B54BEB05E1227B2E0E,
+	ImageTrackingParticles__ctor_m9CEF82DA61D73F1DC11CBA8A61CC04EE94F4E711,
+	ParticleUserInteraction_Start_m1AE227CE17E1F4541F224A899852B7FC64E0F8BF,
+	ParticleUserInteraction_Update_mF559967296F128C26B1C4D155D3F670B0FF366B2,
+	ParticleUserInteraction__ctor_mBF4627C05D9F198643C45E345E405388B27BD6ED,
+};
+static const int32_t s_InvokerIndices[31] = 
+{
+	5179,
+	4187,
+	5274,
+	5274,
+	5274,
+	5274,
+	8381,
+	5220,
+	4220,
+	5274,
+	5274,
+	5274,
+	4081,
+	4187,
+	5274,
+	8381,
+	5274,
+	5274,
+	5274,
+	4091,
+	5274,
+	5274,
+	5274,
+	4091,
+	4187,
+	4187,
+	4187,
+	5274,
+	5274,
+	5274,
+	5274,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	31,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
